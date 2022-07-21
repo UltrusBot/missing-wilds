779ad37ccbde324f3c01ec055f0532bc4a0b99f7
ultra <13109003+UltrusBot@users.noreply.github.com>
2022-07-21 19:44:00 -0400

    Update BYG compat Bump Version Fix #9

--- Fabric/src/main/java/me/ultrusmods/missingwilds/compat/BygModCompat.java
+++ Fabric/src/main/java/me/ultrusmods/missingwilds/compat/BygModCompat.java
@@ -3,33 +3,44 @@ package me.ultrusmods.missingwilds.compat;
 public class BygModCompat extends ModCompat {
 	@Override
 	public void addLogs() {
-		addSimpleLog("aspen_log");
-		addSimpleLog("baobab_log");
-		addSimpleLog("blue_enchanted_log");
-		addSimpleLog("cherry_log");
-		addSimpleLog("cika_log");
-		addSimpleLog("cypress_log");
-		addSimpleLog("ebony_log");
-		addSimpleLog("ether_log");
-		addSimpleLog("fir_log");
-		addSimpleLog("green_enchanted_log");
-		addSimpleLog("holly_log");
-		addSimpleLog("jacaranda_log");
-		addSimpleLog("lament_log");
-		addSimpleLog("mahogany_log");
-		addSimpleLog("mangrove_log");
-		addSimpleLog("maple_log");
-		addSimpleLog("nightshade_log");
-		addSimpleLog("palm_log");
-		addSimpleLog("palo_verde_log");
-		addSimpleLog("pine_log");
-		addSimpleLog("rainbow_eucalyptus_log");
-		addSimpleLog("redwood_log");
-		addSimpleLog("skyris_log");
-		addSimpleLog("bulbis_stem");
-//		addSimpleLog("imparius_stem");    no stripped variant
-		addSimpleLog("sythian_stem");
+		addBygLog("aspen");
+		addBygLog("baobab");
+		addBygLog("blue_enchanted");
+		addBygLog("cherry");
+		addBygLog("cika");
+		addBygLog("cypress");
+		addBygLog("ebony");
+		addBygLog("ether");
+		addBygLog("fir");
+		addBygLog("green_enchanted");
+		addBygLog("holly");
+		addBygLog("jacaranda");
+		addBygLog("lament");
+		addBygLog("mahogany");
+		addBygLog("white_mangrove");
+		addBygLog("maple");
+		addBygLog("nightshade");
+		addBygLog("palm");
+		addBygLog("pine");
+		addBygLog("rainbow_eucalyptus");
+		addBygLog("redwood");
+		addBygLog("skyris");
+		addBygStem("bulbis");
+//		addSimpleLog("imparius");    no stripped variant
+		addBygStem("sythian");
 
+		addSimpleLog("palo_verde_log");
+
+
+	}
+
+
+	public void addBygLog(String logName) {
+		getLogList().add(new LogData("fallen_%s_log".formatted(logName), "%s:%s_log".formatted(getMod(), logName), "%s:block/%s/log".formatted(getMod(), logName), "%s:block/%s/stripped_log".formatted(getMod(), logName)));
+	}
+
+	public void addBygStem(String logName) {
+		getLogList().add(new LogData("fallen_%s_stem".formatted(logName), "%s:%s_stem".formatted(getMod(), logName), "%s:block/%s/log".formatted(getMod(), logName), "%s:block/%s/stripped_log".formatted(getMod(), logName)));
 	}
 
 	@Override
