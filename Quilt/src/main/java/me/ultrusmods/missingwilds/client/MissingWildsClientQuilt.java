3c1e56ffd736c7952771b0fd5b9ecaa59f6400c5
ultra <13109003+UltrusBot@users.noreply.github.com>
2023-01-28 22:05:02 -0500

    Fixed waterlily render layer on quilt

--- Quilt/src/main/java/me/ultrusmods/missingwilds/client/MissingWildsClientQuilt.java
+++ Quilt/src/main/java/me/ultrusmods/missingwilds/client/MissingWildsClientQuilt.java
@@ -4,9 +4,12 @@ import me.ultrusmods.missingwilds.Constants;
 import me.ultrusmods.missingwilds.MissingWildsQuilt;
 import me.ultrusmods.missingwilds.client.render.FireflySwarmRenderer;
 import me.ultrusmods.missingwilds.particle.FireflyParticle;
+import me.ultrusmods.missingwilds.register.MissingWildsBlocks;
 import me.ultrusmods.missingwilds.register.MissingWildsEntities;
+import me.ultrusmods.missingwilds.register.MissingWildsItems;
 import me.ultrusmods.missingwilds.register.MissingWildsParticles;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.minecraft.client.renderer.RenderType;
@@ -28,6 +31,9 @@ public class MissingWildsClientQuilt implements ClientModInitializer {
         }));
         ParticleFactoryRegistry.getInstance().register(MissingWildsParticles.FIREFLY.get(), FireflyParticle.Provider::new);
         EntityRendererRegistry.register(MissingWildsEntities.FIREFLY_SWARM.get(), FireflySwarmRenderer::new);
+
+        ColorProviderRegistry.BLOCK.register((blockState, blockAndTintGetter, blockPos, col) -> blockAndTintGetter != null && blockPos != null ? 2129968 : 7455580, MissingWildsBlocks.WATERLILY_BLOCK.get());
+        ColorProviderRegistry.ITEM.register((itemStack, col) -> 7455580, MissingWildsItems.WATERLILY_ITEM.get());
 
     }
 }
