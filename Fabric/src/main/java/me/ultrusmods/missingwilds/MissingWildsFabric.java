2aceb4c572ac22ada11552ba1db2263c07e7d0a6
ultra <13109003+UltrusBot@users.noreply.github.com>
2023-04-28 21:07:57 -0400

    1.19.4 Update

--- Fabric/src/main/java/me/ultrusmods/missingwilds/MissingWildsFabric.java
+++ Fabric/src/main/java/me/ultrusmods/missingwilds/MissingWildsFabric.java
@@ -19,24 +19,24 @@ public class MissingWildsFabric implements ModInitializer {
     public static final CreativeModeTab MISSING_WILD_ITEMS = FabricItemGroup.builder(
                     new ResourceLocation(Constants.MOD_ID, "items"))
             .icon(() -> new ItemStack(MissingWildsItems.FALLEN_BIRCH_LOG.get()))
-            .displayItems((featureFlagSet, output, bl) -> {
-                output.accept(MissingWildsItems.FALLEN_OAK_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_BIRCH_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_SPRUCE_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_JUNGLE_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_DARK_OAK_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_ACACIA_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_MANGROVE_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_CRIMSON_STEM.get());
-                output.accept(MissingWildsItems.FALLEN_WARPED_STEM.get());
-                output.accept(MissingWildsItems.FALLEN_MUSHROOM_STEM.get());
-                output.accept(MissingWildsItems.BLUE_FORGET_ME_NOT.get());
-                output.accept(MissingWildsItems.PURPLE_FORGET_ME_NOT.get());
-                output.accept(MissingWildsItems.PINK_FORGET_ME_NOT.get());
-                output.accept(MissingWildsItems.WHITE_FORGET_ME_NOT.get());
-                output.accept(MissingWildsItems.SWEETSPIRE.get());
-                output.accept(MissingWildsItems.BROWN_POLYPORE_MUSHROOM.get());
-                output.accept(MissingWildsItems.ROASTED_POLYPORE_MUSHROOM.get());
+            .displayItems((context, entries) -> {
+                entries.accept(MissingWildsItems.FALLEN_OAK_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_BIRCH_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_SPRUCE_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_JUNGLE_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_DARK_OAK_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_ACACIA_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_MANGROVE_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_CRIMSON_STEM.get());
+                entries.accept(MissingWildsItems.FALLEN_WARPED_STEM.get());
+                entries.accept(MissingWildsItems.FALLEN_MUSHROOM_STEM.get());
+                entries.accept(MissingWildsItems.BLUE_FORGET_ME_NOT.get());
+                entries.accept(MissingWildsItems.PURPLE_FORGET_ME_NOT.get());
+                entries.accept(MissingWildsItems.PINK_FORGET_ME_NOT.get());
+                entries.accept(MissingWildsItems.WHITE_FORGET_ME_NOT.get());
+                entries.accept(MissingWildsItems.SWEETSPIRE.get());
+                entries.accept(MissingWildsItems.BROWN_POLYPORE_MUSHROOM.get());
+                entries.accept(MissingWildsItems.ROASTED_POLYPORE_MUSHROOM.get());
             })
             .build();
 
--- Forge/src/main/java/me/ultrusmods/missingwilds/MissingWildsForge.java
+++ Forge/src/main/java/me/ultrusmods/missingwilds/MissingWildsForge.java
@@ -23,24 +23,24 @@ public class MissingWildsForge {
         event.registerCreativeModeTab(new ResourceLocation(Constants.MOD_ID, "items"), builder -> {
             builder.title(Component.translatable("itemGroup.missingwilds.items"));
             builder.icon(() -> new ItemStack(MissingWildsItems.FALLEN_BIRCH_LOG.get()));
-            builder.displayItems((featureFlagSet, output, bl) -> {
-                output.accept(MissingWildsItems.FALLEN_OAK_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_BIRCH_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_SPRUCE_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_JUNGLE_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_DARK_OAK_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_ACACIA_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_MANGROVE_LOG.get());
-                output.accept(MissingWildsItems.FALLEN_CRIMSON_STEM.get());
-                output.accept(MissingWildsItems.FALLEN_WARPED_STEM.get());
-                output.accept(MissingWildsItems.FALLEN_MUSHROOM_STEM.get());
-                output.accept(MissingWildsItems.BLUE_FORGET_ME_NOT.get());
-                output.accept(MissingWildsItems.PURPLE_FORGET_ME_NOT.get());
-                output.accept(MissingWildsItems.PINK_FORGET_ME_NOT.get());
-                output.accept(MissingWildsItems.WHITE_FORGET_ME_NOT.get());
-                output.accept(MissingWildsItems.SWEETSPIRE.get());
-                output.accept(MissingWildsItems.BROWN_POLYPORE_MUSHROOM.get());
-                output.accept(MissingWildsItems.ROASTED_POLYPORE_MUSHROOM.get());
+            builder.displayItems((context, entries) -> {
+                entries.accept(MissingWildsItems.FALLEN_OAK_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_BIRCH_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_SPRUCE_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_JUNGLE_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_DARK_OAK_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_ACACIA_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_MANGROVE_LOG.get());
+                entries.accept(MissingWildsItems.FALLEN_CRIMSON_STEM.get());
+                entries.accept(MissingWildsItems.FALLEN_WARPED_STEM.get());
+                entries.accept(MissingWildsItems.FALLEN_MUSHROOM_STEM.get());
+                entries.accept(MissingWildsItems.BLUE_FORGET_ME_NOT.get());
+                entries.accept(MissingWildsItems.PURPLE_FORGET_ME_NOT.get());
+                entries.accept(MissingWildsItems.PINK_FORGET_ME_NOT.get());
+                entries.accept(MissingWildsItems.WHITE_FORGET_ME_NOT.get());
+                entries.accept(MissingWildsItems.SWEETSPIRE.get());
+                entries.accept(MissingWildsItems.BROWN_POLYPORE_MUSHROOM.get());
+                entries.accept(MissingWildsItems.ROASTED_POLYPORE_MUSHROOM.get());
             });
         });
 
